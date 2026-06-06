f20df8e49fb89d5656beefaa0371a6f988865b68
s01071233604-tech <s01071233604@gmail.com>
2026-06-06 17:16:36 +0800

    Switch sitemap to Route Handler for reliable Google access

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" encoding="UTF-8"?>
+import { NextResponse } from "next/server";
+
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://safejson.vercel.app/</loc>
@@ -42,4 +44,13 @@
     <changefreq>yearly</changefreq>
     <priority>0.3</priority>
   </url>
-</urlset>
+</urlset>`;
+
+export async function GET() {
+  return new NextResponse(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600",
+    },
+  });
+}
